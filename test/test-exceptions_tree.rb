3a08966e6619190ad1e889f8e0da3c1a4b70cdb0
Alan Stebbens <alan.stebbens@procore.com>
2016-10-11 13:45:45 -0700

    remove monkey-patch; use FP call methods

--- test/test-exceptions_tree.rb
+++ test/test-exceptions_tree.rb
@@ -3,35 +3,20 @@ require 'exceptions_tree'
 
 class TestExceptionsTree < Minitest::Test
 
+  STANDARD_EXCEPTIONS = [ ::RuntimeError, ::StandardError, ::IOError, ::IndexError,
+                          ::RegexpError, ::RangeError, ::TypeError, ::SystemCallError ]
+
   def test_exceptions_tree_class_methods
     assert_respond_to ExceptionsTree, :build_exceptions_tree
     assert_respond_to ExceptionsTree, :inspect_exceptions_tree
     assert_respond_to ExceptionsTree, :display_exceptions
   end
 
-  class ::Hash
-
-    def is_a_deep_key?(key)
-      self.key?(key) || self.is_a_subkey?(key)
-    end
-
-    def is_a_subkey?(key)
-      self.select{|k,v| v.respond_to?(:key?)}.each do |k,v|
-        return true if v.is_a_deep_key?(key)
-      end
-      false
-    end
-
-  end
-
-  STANDARD_EXCEPTIONS = [ ::RuntimeError, ::StandardError, ::IOError, ::IndexError,
-                          ::RegexpError, ::RangeError, ::TypeError, ::SystemCallError ]
-
   def test_build_exceptions_tree
     tree = ExceptionsTree.build_exceptions_tree
     assert_kind_of Hash, tree
     STANDARD_EXCEPTIONS.each do |exception_class|
-      assert tree.is_a_deep_key?(exception_class)
+      assert is_a_deep_key?(tree, exception_class)
     end
   end
 
@@ -48,6 +33,19 @@ class TestExceptionsTree < Minitest::Test
     output = ExceptionsTree.inspect_exceptions_tree(tree, 0)
     assert_output(output) { ExceptionsTree.display_exceptions }
   end
+
+  private
+
+    def is_a_deep_key?(tree, key)
+      tree.key?(key) || is_a_subkey?(tree, key)
+    end
+
+    def is_a_subkey?(tree, key)
+      tree.select{|k,v| v.respond_to?(:key?)}.each do |k,v|
+        return true if is_a_deep_key?(v, key)
+      end
+      false
+    end
 
 end
 
